c1b9a2c30e6b0f06ebfe0b2188a24cc7e3e7b6e9
Cory Chang <changcory@gmail.com>
2017-10-08 22:04:24 -0700

    update email

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ $(window).load(function() {
 	content.append($("<p>Hi, I'm Cory, a Computer Science and Human Computer Interaction double major at Carnegie Mellon University. I have a passion for solving problems and working on awesome projects.</p>"));
 	content.append($("<p>In my free time, I enjoy working on game projects. A sampling of my work can be found under the \"Games\" tab. A selection of interesting courses that I've taken can also be found under \"Courses.\"</p>"));
 	content.append($("<p>I also really enjoy film. Currently, my favorite movie is <i>The Shawshank Redemption</i>, and my favorite TV series is <i>The Wire</i>.</p>"));
-	content.append($("<p>Contact: <a href='mailto:changcory@gmail.com'>changcory@gmail.com</a></p>"));
+	content.append($("<p>Contact: <a href='mailto:coryc@alumni.cmu.edu'>coryc@alumni.cmu.edu</a></p>"));
 	content.append($("<br/>"));
 	content.append($("<p><i>Last updated 9/10/14</i></p>"));
 	//content.width(580);
